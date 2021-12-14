f378e180ccae04dca208c4242599b121996df081
Aric McAlister <aricmca@gmail.com>
2021-12-14 13:40:21 -0600

    commit tree 751bae2cb86d7aedd2efde000587f0a0098d3f92

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find_by(id: params[:user_id])
+    items = user.items
     render json: user, include: :items
   end
 
